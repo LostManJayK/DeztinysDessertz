95b34030a251dfc743165b526c7da1fb463dd88e
Andrew Spence <andrewspence@Andrews-MBP.lan>
2024-04-26 18:23:31 -0700

    Initial commit for branch

--- django_apps/products/models.py
+++ django_apps/products/models.py
@@ -6,9 +6,17 @@ from django.db import models
 class MenuItem:
 
     def __init__(self):
-
+        
         item_options = [] #For storing the options needed for selection
 
+
+
+#ItemOption model
+class ItemOption:
+
+    def __init__(self):
+
+        pass
 
     #Create the HTML to populate the MenuItem
     def construct_items(self):
