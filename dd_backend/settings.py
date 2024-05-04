230b3739d9c6d55caf53e1e4c6dcfaaeed726f21
Andrew Spence <andrewspence@Andrews-MBP.lan>
2024-05-03 18:41:31 -0700

    Updated database settings

--- dd_backend/settings.py
+++ dd_backend/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'dd_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': '3306',
     }
+
+    },
+
+
 }
 
 
