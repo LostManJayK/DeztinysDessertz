267c409d68b2de3555b194b7139e5c9a5f845e2f
Andrew Spence <andrewspence@Andrews-MBP.lan>
2024-05-03 18:58:40 -0700

    DB settings update

--- dd_backend/settings.py
+++ dd_backend/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS"),
         'HOST': os.environ.get("DB_HOST"),
-        'PORT': '3306',
     }
 }
 
